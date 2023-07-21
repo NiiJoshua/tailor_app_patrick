65420eb01c209172957abbb5647ba1327499f5c1
NiJoshua <joshkobeng@gmail.com>
2023-07-21 12:29:47 +0000

    adding a body to script

--- app.py
+++ app.py
@@ -33,3 +33,41 @@ def search_data(client_name):
     data = cursor.fetchall()
     conn.close()
     return data
+
+# Code Body
+def main():
+    with st.sidebar:
+        st.write("Welcome to Patricks Tailor shop")
+
+    mode = st.sidebar.radio('Choose an option',['Input Data', 'Search Record'])
+
+    if mode == 'Input Data':
+        st.header("Client Data Entry")
+        st.write("Enter user information below:")
+
+        # Input fields 
+        client_name = st.text_input("Name of Cleint")
+        residence = st.text_input("Where does the client live")
+        arm_length = st.number_input("arm_length (in cm)", min_value=0, step=1)
+        shoulder_length = st.number_input("Height (in cm)", min_value=0, step=1)
+
+        # Submit button to record user information
+        if st.button("Submit"):
+            create_table()  # Create the table if it doesn't exist
+            insert_data(client_name, residence, arm_length, shoulder_length)
+            st.success("User information recorded successfully!")
+
+    # Search data by name
+    if mode == 'Input Data':
+        search_name = st.text_input("Search User by Name")
+        if st.button("Search"):
+            data = search_data(search_name)
+            if data:
+                st.write("User Information:")
+                for user in data:
+                    st.write(f"Name: {user[0]}, arm_length: {user[3]}, shoulder_length: {user[3]}")
+            else:
+                st.write("No user found with the given name.")
+
+if __name__ == "__main__":
+    main()
