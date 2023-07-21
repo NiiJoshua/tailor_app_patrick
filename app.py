e01d2319fa1edef64cf094aa58d38ad44f0ecf26
NiJoshua <joshkobeng@gmail.com>
2023-07-21 09:52:40 +0000

    adding a function to create the table using sqlite3

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
 import sqlite3
+
+# Function to create a database table
+def create_table():
+    conn = sqlite3.connect("user_data.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        client_name TEXT,
+                        residence TEXT,
+                        arm_length INTEGER,
+                        shoulder_length INTEGER
+                   )
+                   """)
